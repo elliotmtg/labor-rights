7eba7b4d90c0feb3595dfa7703bb7762b53178c9
ngm01 <nathaniel.g.moore@gmail.com>
2018-04-28 09:02:53 -0400

    large check marks

--- labor-rights-web/src/Verifier.js
+++ labor-rights-web/src/Verifier.js
@@ -18,8 +18,10 @@ class Verifier extends Component{
             <label>
             {this.props.stakeHolderName}'s Soveriegn ID:
             <input type="text" value={this.props.stakeHolderID} onChange={this.handleID.bind(this)}/>
-            <span role="img" alt="X mark" id="x-mark">&#10006;</span>
-            <span role="img" alt="check mark" id="check-mark">&#10004;</span>
+            <div id="approval">
+                <span role="img" alt="X mark" id="x-mark">&#10006;</span>
+                <span role="img" alt="check mark" id="check-mark">&#10004;</span>
+            </div>
           </label>
 
         );
