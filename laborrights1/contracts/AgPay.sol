cad21f05a6532eaee0b33a769744f86a8fd6a4b8
bloya <l.loya.b@gmail.com>
2018-04-28 18:54:46 -0400

    record event

--- laborrights1/contracts/AgPay.sol
+++ laborrights1/contracts/AgPay.sol
@@ -11,18 +11,36 @@ contract AgPay {
         bool approvedBychecker;
         bool approvedByworker;
         string pieceCondition;
+        string employmentContract;
         uint pieceWeight;
         uint pueceRate;
         uint date;
+
     }
 
     // this data is all publicly explorable
     mapping(bytes32 => pieceEntry) public pieceEntries;
 
-    // for event listeners    
-    event LogApprovedDocument(address approver, bytes32 docHash);
+    //record
+    event Record(address checker, address worker,bool approvedBychecker, bool approvedByworker,
+        string pieceCondition, string employmentContract, uint pieceWeight, uint pueceRate,
+        uint date);
+        
+    function record(address checker, address worker, bool approvedBychecker, bool approvedByworker,
+        string pieceCondition, string employmentContract, uint pieceWeight, uint pueceRate,
+        uint date) public {
+        registry[msg.sender] = pieceEntry(checker,
+        worker,
+        approvedBychecker,
+        approvedByworker,
+        pieceCondition,
+        employmentContract,
+        pieceWeight,
+        pueceRate,
+        date);
+    }
+    mapping (address => pieceEntry) public registry;
     
-    //add view function
- 
-    
+    //view fx
+
 }
